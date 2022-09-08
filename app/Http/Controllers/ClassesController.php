650155ff1024b9be6512c4fb967b894355fa873b
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2022-09-08 14:34:06 +0300

    Création des controller de Classes, Filieres, Etuidants

---
+++ app/Http/Controllers/ClassesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classes;
+use Illuminate\Http\Request;
+
+class ClassesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Classes  $classes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Classes $classes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Classes  $classes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Classes $classes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Classes  $classes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Classes $classes)
+    {
+        //
+    }
+}
